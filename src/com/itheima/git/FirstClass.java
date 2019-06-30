4df485d4b04c85cd1480fe287d2de2e4e73d6022
zhi <zhi@163.com>
2019-06-30 19:25:29 +0800

    把本地工程添加到commit

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -4,4 +4,10 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("aaaaa");
     }
+
+    public void test(){
+        int i =0;
+        System.out.println(i);
+
+    }
 }
